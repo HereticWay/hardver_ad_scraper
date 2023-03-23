4cc19d82bba57f5eeeb84d456a9ef963fbc0cb42
HereticWay <vpleft@gmail.com>
2023-03-23 15:40:07 +0100

    add project files;

---
+++ ad_extractor.py
@@ -0,0 +1,104 @@
+# My simple messy script to get all ads from a harverapro link
+# By Ádám Madar
+
+from json import JSONEncoder
+from typing import Any
+from urllib.request import urlopen
+from bs4 import BeautifulSoup, Tag
+
+
+class Ad:
+    __slots__ = (
+        'title',
+        'link',
+        'price',
+        'is_frozen',
+        'city',
+        'seller_name',
+        'seller_rating'
+    )
+
+    def __init__(self, title: str, link: str, price: str, is_frozen: bool, city: str, seller_name: str, seller_rating: str) -> None:
+        self.title = title
+        self.link = link
+        self.price = price
+        self.is_frozen = is_frozen
+        self.city = city
+        self.seller_name = seller_name
+        self.seller_rating = seller_rating
+
+    def __eq__(self, __o: object) -> bool:
+        if isinstance(__o, Ad):
+            return self.link == __o.link
+
+        return False
+
+
+class AdJSONEncoder(JSONEncoder):
+    def default(self, o: Ad) -> dict[str, Any]:
+        return dict(
+            zip(
+                o.__slots__,
+                [
+                    o.title,
+                    o.link,
+                    o.price,
+                    o.is_frozen,
+                    o.city,
+                    o.seller_name,
+                    o.seller_rating
+                ]))
+
+
+class AdExtractor:
+    """A simple class to get all the deals from a hardverapro.hu link"""
+
+    def __init__(self, url: str) -> None:
+        self.__url: str = url
+
+    def get_ads(self) -> list[Ad]:
+        content = urlopen(self.__url).read()
+        soup = BeautifulSoup(content, features='html.parser')
+        ad_container = soup.find('div', attrs={'class': 'uad-list'})
+        ads_list: list[Ad] = []
+
+        # Return if there is no data to process
+        if not isinstance(ad_container, Tag):
+            return []
+
+        rows = ad_container.find_all(attrs={'class': 'media-body'})
+        for row in rows:
+            # If it's a sponsored ad
+            if 'featured' in row.parent['class']:
+                # Then skip it
+                continue
+
+            # Note: something(...) is equal to something.find_all(...)!
+            title_col = row.find(attrs={'class': 'uad-title'})
+            title = title_col.a.text
+            link = title_col.a['href']
+            is_frozen = False
+            if title_col.p:
+                is_frozen = True
+
+            info_col = row.find(attrs={'class': 'uad-info'})
+            price = info_col.find(attrs={'class': 'uad-price'}).text
+            city = info_col.find(attrs={'class': 'uad-light'}).text
+
+            misc_col = row.find(attrs={'class': 'uad-misc'})
+            seller_data = misc_col.find_all(attrs={'class': 'uad-light'})
+            seller_name = seller_data[0].a.text
+            seller_rating = seller_data[1].span.text
+
+            ad = Ad(
+                title=title,
+                link=link,
+                price=price,
+                is_frozen=is_frozen,
+                city=city,
+                seller_name=seller_name,
+                seller_rating=seller_rating,
+            )
+            ads_list.append(ad)
+
+        return ads_list
---
+++ ad_manager.py
@@ -0,0 +1,43 @@
+from os import path
+import json
+from ad_extractor import AdExtractor, Ad, AdJSONEncoder
+
+ADS_FILE = './ads.json'
+URL = (
+    'https://hardverapro.hu/aprok/mobil/mobil/keres.php?stext=poco+x3+nfc&stcid_text='
+    '&stcid=&stmid_text=&stmid=&minprice=&maxprice=&cmpid_text=&cmpid=&usrid_text=&usrid='
+    '&__buying=0&__buying=1&stext_none=')
+
+
+class AdManager:
+    __slots__ = (
+        'ads',
+        'ad_extractor'
+    )
+
+    def __init__(self) -> None:
+        self.ads = self.load_ads() or []
+        self.ad_extractor = AdExtractor(url=URL)
+
+    def load_ads(self) -> list[Ad] | None:
+        if not path.exists(ADS_FILE):
+            return None
+
+        ads: list[Ad] = []
+        with open(ADS_FILE, 'r') as file:
+            for ad_dict in json.loads(file.readline()):
+                ads.append(Ad(**ad_dict))
+        return ads
+
+    def save_ads(self) -> None:
+        with open(ADS_FILE, 'w') as file:
+            file.write(json.dumps(self.ads, cls=AdJSONEncoder))
+
+    def update_ads_and_return_new_ones(self) -> list[Ad]:
+        ads = self.ad_extractor.get_ads()
+
+        new_ads: list[Ad] = [ad for ad in ads if ad not in self.ads]
+        self.ads = ads
+
+        return new_ads
+
