abfcab7e4fcd91961d470999b07d1a428cc6dc3e
HereticWay <vpleft@gmail.com>
2023-05-11 20:29:47 +0200

    add new variables to Ad;

--- hardver_ad_scraper/ad.py
+++ hardver_ad_scraper/ad.py
@@ -13,25 +13,29 @@ class Ad:
     #     'seller_rating'
     # )
 
-    def __init__(self, title: str, link: str, price: str, is_frozen: bool, city: str, seller_name: str, seller_rating: str) -> None:
+    def __init__(self, title: str, image_url: str, link: str, price: str, is_frozen: bool, city: str, seller_name: str, seller_rating_positive: str, seller_rating_negative: str) -> None:
         self.title = title
+        self.image_url = image_url
         self.link = link
         self.price = price
         self.is_frozen = is_frozen
         self.city = city
         self.seller_name = seller_name
-        self.seller_rating = seller_rating
+        self.seller_rating_positive = seller_rating_positive
+        self.seller_rating_negative = seller_rating_negative
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, Ad):
             return (
                 self.title == other.title and
+                self.image_url == other.image_url and
                 self.link == other.link and
                 self.price == other.price and
                 self.is_frozen == other.is_frozen and
                 self.city == other.city and
-                self.seller_name == other.seller_name and
-                self.seller_rating == other.seller_rating)
+                self.seller_name == other.seller_name)
+                # self.seller_rating_positive == other.seller_rating_positive and
+                # self.seller_rating_negative == other.seller_rating_negative)
 
         return False
 
@@ -41,12 +45,14 @@ class AdJSONEncoder(json.JSONEncoder):
         if isinstance(obj, Ad):
             return {
                 'title': obj.title,
+                'image_url': obj.image_url,
                 'link': obj.link,
                 'price': obj.price,
                 'is_frozen': obj.is_frozen,
                 'city': obj.city,
                 'seller_name': obj.seller_name,
-                'seller_rating': obj.seller_rating
+                'seller_rating_positive': obj.seller_rating_positive,
+                'seller_rating_negative': obj.seller_rating_negative
             }
         else:
             return super().default(obj)
@@ -59,9 +65,11 @@ class AdJSONDecoder(json.JSONDecoder):
     def object_hook(self, obj):
         return Ad(
             obj['title'],
+            obj['image_url'],
             obj['link'],
             obj['price'],
             obj['is_frozen'],
             obj['city'],
             obj['seller_name'],
-            obj['seller_rating'])
+            obj['seller_rating_positive'],
+            obj['seller_rating_negative'])
