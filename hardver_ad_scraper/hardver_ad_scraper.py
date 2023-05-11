1e5d2717422e3e45ff4c4fac62728c93466a9080
HereticWay <vpleft@gmail.com>
2023-05-11 21:01:53 +0200

    fix typos; simplify code a bit;

--- hardver_ad_scraper/hardver_ad_scraper.py
+++ hardver_ad_scraper/hardver_ad_scraper.py
@@ -13,14 +13,14 @@ class HardverAdScraper:
     def get_ads(self) -> list[Ad]:
         content = urlopen(self.__url).read()
         soup = BeautifulSoup(content, features='html.parser')
-        ad_container = soup.find('div', attrs={'class': 'uad-list'})
+        ad_container = soup.find('div', class_='uad-list')
         ads_list: list[Ad] = []
 
         # Return if there is no data to process
         if not isinstance(ad_container, Tag):
             return []
 
-        rows = ad_container.find_all(attrs={'class': 'media'})
+        rows = ad_container.find_all(class_='media')
         for row in rows:
             # If it's a sponsored ad
             if 'featured' in row['class']:
@@ -29,25 +29,25 @@ class HardverAdScraper:
 
             image_url = f'https:{row.img["data-retina-url"]}'
 
-            row_body = row.find(attrs={'class': 'media-body'})
+            row_body = row.find(class_='media-body')
 
             # Note: something(...) is equal to something.find_all(...)!
-            title_col = row_body.find(attrs={'class': 'uad-title'})
+            title_col = row_body.find(class_='uad-title')
             title = title_col.a.text.strip()
             link = title_col.a['href'].strip()
             is_frozen = False
             if title_col.p:
                 is_frozen = True
 
-            info_col = row_body.find(attrs={'class': 'uad-info'})
-            price = info_col.find(attrs={'class': 'uad-price'}).text.strip()
-            city = info_col.find(attrs={'class': 'uad-light'}).text.strip()
+            info_col = row_body.find(class_='uad-info')
+            price = info_col.find(class_='uad-price').text.strip()
+            city = info_col.find(class_='uad-light').text.strip()
 
-            misc_col = row_body.find(attrs={'class': 'uad-misc'})
-            seller_data = misc_col.find_all(attrs={'class': 'uad-light'})
+            misc_col = row_body.find(class_='uad-misc')
+            seller_data = misc_col.find_all(class_='uad-light')
             seller_name = seller_data[0].a.text.strip()
-            positive_rating_span = seller_data[1].find(attrs={'class': 'uad_rating_positive'})
-            negative_rating_span = seller_data[1].find(attrs={'class': 'uad_rating_negative'})
+            positive_rating_span = seller_data[1].find(class_='uad-rating-positive')
+            negative_rating_span = seller_data[1].find(class_='uad-rating-negative')
             seller_rating_positive = positive_rating_span.text.strip() if positive_rating_span else 'N.A.'
             seller_rating_negative = negative_rating_span.text.strip() if negative_rating_span else 'N.A.'
 
